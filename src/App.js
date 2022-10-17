61ff8da0cd4f8c5aabc45b37477dde03f1d047f9
LY Lee <quirkidigital@gmail.com>
2022-10-17 15:32:47 +0800

    commit assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//App is class-component
+class App extends Component { 
+  state = {
+    lastName: 'Lee',
+    designation: 'volunteer',
+    affliation: 'Pawrents Club',
+    contact:['91234567','iluHamsters@example.com'],
+    hamstersForAdoption: [{
+      name: 'Sofie',
+      ageInMonths: 7,
+      breed: 'Winter White',
+      gender: 'female',
+      Adopted: false
+    },{
+      name: 'Charles',
+      ageInMonths: 12,
+      breed: 'Syrian',
+      gender: 'male',
+      Adopted: false
+    },{
+      name: 'Peanut',
+      ageInMonths: 2,
+      breed:'Roborovski',
+      gender: 'male',
+      Adopted: false
+    }]
+  }
+
+  profileUpdate = () => {
+    this.setState({
+      designation: 'fosterer',
+      affliation: 'Hamster Society'
+    })
+  }
+  render() {
+    return (
+      <div class ="text-center mt-5">
+        <p><b>PROFILE</b></p>
+        <div>Ms {this.state.lastName} is a {this.state.designation} affliated with {this.state.affliation}.</div>
+        <div>She currently has {this.state.hamstersForAdoption.length} hamsters up for adoption.</div><br/>
+        <button class = "mb-3" onClick={this.profileUpdate}>Update Profile</button>
+        <p><b>Hamsters for Adoption</b></p>
+        {this.state.hamstersForAdoption.map((e => <div>Name: {e.name}, {e.gender} {e.breed}, {e.ageInMonths} months old</div>))}
+        <br/>
+        <div><b>CONTACT INFORMATION</b></div>
+        {this.state.contact.map((e) => {return (<div>{e}</div>)})}
+      </div>     
+    )
+  }
 }
 
 export default App;
